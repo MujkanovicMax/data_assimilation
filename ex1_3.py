4a567cd12188aab319a5c891d29e3fdd7e826007
Mujkanovic.Max <mujkanovic.max@hotmail.de>
2019-05-13 17:03:20 +0200

    1_3d

---
+++ ex1_3.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+s = 5.67051*1e-8
+s_appr = 5*1e-8
+
+T1 = 10 + 10*np.sin(8*np.pi*0.5/360.)
+T2 = 10 + 10*np.sin(8*np.pi*10.5/360.)
+T3 = 10 + 10*np.sin(8*np.pi*100/360.)
+
+instr_e = (s*pow(T1,4)-5.2*1e-4+s*pow(T2,4)-4.9*1e-3+s*pow(T3,4)-8*1e-4)/3
+
+lamda = np.arange(0,360,1)
+
+T = 10 + 10*np.sin(8*np.pi*lamda/360.)
+
+rad_grid = np.zeros(T.shape)
+
+for i in range(1,11):
+    
+    y = (4.9*1e-3-5.2*1e-4)/10. * i + 5.2*1e-4 - (4.9*1e-3-5.2*1e-4)/10.*0.5
+    rad_grid[i] = y
+for i in range(11,101):
+    
+    y = (8*1e-4-4.9*1e-3)/(100-10.5) * i + 4.9*1e-3 - (8*1e-4-4.9*1e-3)/(100-10.5)*10.5
+    rad_grid[i] = y
+    
+for i in range(101,361):
+    
+    y = (5.2*1e-4-8*1e-4)/(60.5) * i + 8*1e-4 - (5.2*1e-4-8*1e-4)/(60.5)*100
+    
+    if i == 360:
+        i = 0
+    
+    rad_grid[i] = y
+
+unr_e = np.mean(np.power(T,4)*s-rad_grid)
+unr_e_alt = np.mean(np.power(T,4)[::3]*s-rad_grid[::3])
+
+
+op_e = np.mean(rad_grid-rad_grid/s*s_appr)
+
+total_e = instr_e + unr_e + op_e
+
+rel_instr_e = instr_e/total_e
+rel_unr_e = unr_e/total_e
+rel_op_e = op_e/total_e
+
+
+
+
