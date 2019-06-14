1ba872728b7f2b9105327ca99ea07fca47c99ca0
Max M <mujki@hotmail.de>
2019-06-14 14:42:55 +0200

    B,R kann berechnet werden; analyseberechnung begonnen

--- project/LWM_main.py
+++ project/LWM_main.py
@@ -50,7 +50,7 @@ for i in range(t-1):
 ### Anmerkung: truth enthält von 0 bis n-1 die höhe und von n bis 2n-1 die geschwindigkeit u
 
 h = truth[0:n,:]
-u = truth[n:2*n,:]
+u = truth[n:2*n,:]              #######Boundary condition is u(x = last element) = 0
 
 
 #h_alt = truth_alt[0:n,:]
@@ -59,25 +59,70 @@ u = truth[n:2*n,:]
 ###Observations
 ##all gridpoints obs###
 sig_h = 0.01
-h_err = h*np.random.normal(0,sig_h)
-h_obs = h + h_err
+h_err = h[:,-1]*np.random.normal(0,sig_h)
+h_obs = h[:,-1] + h_err
 
 sig_u = 0.01
-u_err = u*np.random.normal(0,sig_u)
-u_obs = u + u_err
+u_err = u[:,-1]*np.random.normal(0,sig_u)
+u_obs = u[:,-1] + u_err
 
 R = np.diag(np.append(h_err,u_err))
 H = np.identity(2*n)
 
 ###mth gridpoint observed###
-m = 3
-h_obs = h[::m] + h_err[::m]
-u_obs = u[::m] + u_err[::m]
-R = np.diag(np.append(h_err[::m],u_err[::m]))
-H = np.zeros((int(np.ceil(n/(m+0.0))),n))
-for i in range(H.shape[0]):
-    H[i,m*i] = 1
-H = np.hstack((H,H))
+#m = 3
+#h_obs = h[::m] + h_err[::m]
+#u_obs = u[::m] + u_err[::m]
+#R = np.diag(np.append(h_err[::m],u_err[::m]))
+#H = np.zeros((int(np.ceil(n/(m+0.0))),n))
+#for i in range(H.shape[0]):
+    #H[i,m*i] = 1
+#H = np.hstack((H,H))                                    ### mit korrelation testen, dann aber korrelation ignorieren
+
+#### only u/h observed at every gridpont
+#u_obs = u[::m] + u_err[::m]
+#R = np.diag(u_err)
+#H=np.identity(n)
+
+###B matrix
+
+init = truth[:,-1]
+n_fc = 50
+nsteps1 = 10
+nsteps2 = 40
+tmp_B1 = np.ones((2*n,n_fc))
+tmp_B2 = np.ones((2*n,n_fc))
+for i in range(n_fc):
+    for j in range(nsteps1):
+        init = channel(init,dt,sig)
+    tmp_B1[:,i]=init   
+for i in range(n_fc):
+    init=tmp_B1[:,i]
+    for j in range(nsteps2):
+        init = channel(init,dt,sig)
+    tmp_B2[:,i]=init 
+
+diff = tmp_B2-tmp_B1
+B=np.zeros((2*n,2*n))
+for i in range (n_fc):
+    B=B+np.outer(diff[:,i],diff[:,i])
+B = B/n_fc
+
+
+n_assim = 5
+
+obs_error = truth[:,-n_assim:]* np.random.normal(0,sig_h,size=truth[:,-n_assim:].shape)
+obs = np.mean(truth[:,-n_assim:] + obs_error,axis=1)
+
+bg_error = truth[:,-n_assim:]* np.random.normal(0,sig_h,size=truth[:,-n_assim:].shape)
+bg = np.mean(truth[:,-n_assim:] + bg_error,axis=1)
+
+def J(x):
+    J = np.dot(np.dot((x-bg),np.linalg.inv(B)),(x-bg)) + np.dot(np.dot((obs-np.dot(H,x)),np.linalg.inv(R)),(obs-np.dot(H,x)))
+    return J
+
+a = J(truth[:,-1])    
+
 
 #Generate a visual simulation of the two variables
 #f, (ax1, ax2) = plt.subplots(2, sharex=True,figsize=(15,15))
